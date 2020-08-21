8d539a372f620eaebf1815faed17753ab09908b9
Alex <alex.p.flores17@gmail.com>
2020-08-21 00:53:06 -0500

    home v0.2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,25 @@ import arrow from '../images/arrow-right.svg';
 
 export const Home = () => {
     return (
-        <div>
+        <div className="col-12">
             <div className="home-video">
                 <video autoPlay muted loop className="myVideo" type="video/mp4" playsInline>
                     <source src={LSDbackground} />
                 </video>
             </div>
-            <div>
-                <h1>Hola</h1>
-            </div>
+
+            <div className="home-container col-md-6 col-12">
+                <h5 className="animate__animated animate__fadeInUp animate__slow">
+                    Retamos el sentido de lo establecido con propuestas innovadoras en ciencia, arte y tecnología
+                </h5>   
+                <div className="btn-row animate__animated animate__fadeIn animate__delay-2s ">
+                    <button className="explore-button" >Explore
+                        <div className="arrow-icon" >
+                        <img src={arrow} alt="row"/>
+                        </div>
+                    </button>                 
+                </div>             
+            </div>  
         </div>
     )
 }
