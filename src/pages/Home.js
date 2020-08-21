07643846a5a8276bba18a6a0ecc2e0b0140c0592
Alex <alex.p.flores17@gmail.com>
2020-08-21 00:32:56 -0500

    home v 0.1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import LSDbackground from '../video/backgroundweb.mp4';
+import arrow from '../images/arrow-right.svg';
 
 export const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <div className="home-video">
+                <video autoPlay muted loop className="myVideo" type="video/mp4" playsInline>
+                    <source src={LSDbackground} />
+                </video>
+            </div>
+            <div>
+                <h1>Hola</h1>
+            </div>
         </div>
     )
 }
