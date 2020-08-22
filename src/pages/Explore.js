4a4f1d9ef9366517b625277c1ec7d298b03571f9
dstprojects <36209165+dstprojects@users.noreply.github.com>
2020-08-22 03:29:54 -0500

    pruebas responsive

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -30,29 +30,7 @@ export const Explore = () => {
 
     return (
             <div className="explore-container">
-                <div className='row'>
-                    <div className='explore-texto col-sm-6 col-md-5'>
-                        <nav>
-                            <ul>
-                            <li>
-                                <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Light">Light</Link>
-                            </li>
-                            <li>
-                                <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Sound">& Sound</Link>
-                            </li>
-                            <li>
-                                <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Disruptive">Disruptive</Link>
-                            </li>
-                            <li>
-                                <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Lab">Lab</Link>
-                            </li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div className="division-services col-sm-6 col-md-7" >
-                            <ExploreRoutes />
-                    </div>
-                </div>   
+                  
             </div>
     )
 }
