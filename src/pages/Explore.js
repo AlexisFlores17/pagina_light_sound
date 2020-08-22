aa8507e5f1101bcb7df6638147b55d6e49ddf008
Alex <alex.p.flores17@gmail.com>
2020-08-22 03:01:35 -0500

    explore2

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -29,33 +29,30 @@ export const Explore = () => {
     } 
 
     return (
-            <div className="explore-container col-12">
-                <div  className="menu-secondary-background-color"></div>
-                <div  className="menu-layer">
-                    <div className='row col-xl-12 col-sm-6'>
-                        <div className='explore-texto col-sm-6 col-md-5 col-xl-6'>
-                            <nav>
-                                <ul>
-                                <li>
-                                    <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Light">Light</Link>
-                                </li>
-                                <li>
-                                    <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Sound">& Sound</Link>
-                                </li>
-                                <li>
-                                    <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Disruptive">Disruptive</Link>
-                                </li>
-                                <li>
-                                    <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Lab">Lab</Link>
-                                </li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <div className="division-services col-sm-6 col-md-7 col-xl-6" >
-                                <ExploreRoutes />
-                        </div>
-                    </div>   
-                </div>
+            <div className="explore-container">
+                <div className='row'>
+                    <div className='explore-texto col-sm-6 col-md-5'>
+                        <nav>
+                            <ul>
+                            <li>
+                                <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Light">Light</Link>
+                            </li>
+                            <li>
+                                <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Sound">& Sound</Link>
+                            </li>
+                            <li>
+                                <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Disruptive">Disruptive</Link>
+                            </li>
+                            <li>
+                                <Link onMouseEnter={e => handleHover(e)} onMouseOut={e => handleHoverExit(e)} to="/explore/Lab">Lab</Link>
+                            </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="division-services col-sm-6 col-md-7" >
+                            <ExploreRoutes />
+                    </div>
+                </div>   
             </div>
     )
 }
