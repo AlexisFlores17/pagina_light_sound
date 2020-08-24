baa073aba1462c7b9543a02fad4b83d151367c42
dstprojects <36209165+dstprojects@users.noreply.github.com>
2020-08-24 03:19:05 -0500

    scroll to top en contacto y showreel

--- src/pages/Showreel.js
+++ src/pages/Showreel.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+function scrollTop () {
+
+    window.scroll(0,0);
+
+}
+
 export const Showreel = () => {
     return (
         <div className='showreel-container'>
@@ -8,7 +14,7 @@ export const Showreel = () => {
                 <h1>Titulo Showreel</h1>
                 <p>Consectetur et excepteur ullamco consequat. Deserunt duis id duis amet non non sint. Fugiat pariatur ullamco eiusmod esse sit anim ea reprehenderit commodo anim et ipsum sunt est. Ex laborum ad nisi mollit irure.</p>
             </div>
-            <Link to="/Showreel/info">Ver mas</Link>
+            <Link to="/Showreel/info" onClick={scrollTop}>Ver mas</Link>
         </div>
     )
 }
