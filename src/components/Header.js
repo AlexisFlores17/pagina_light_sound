7c757ce7bff283fd22d41334080401b1053c5d70
dstprojects <36209165+dstprojects@users.noreply.github.com>
2020-08-31 21:48:52 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = ({ history}) => {
   return (
     <div className= "header-navbar lax" data-lax-opacity="0 1, (vh/2) 0">
       <Navbar bg="none" expand="lg" variant="dark" className="navbar-container">
-        <Navbar.Brand href="#home">LSD LAB.</Navbar.Brand>
+        <Navbar.Brand href="/#home">LSD LAB.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
