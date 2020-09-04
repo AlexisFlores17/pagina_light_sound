0c261a5b580b8feb4c2d31887d2480666f42da96
dstprojects <36209165+dstprojects@users.noreply.github.com>
2020-09-04 02:13:41 -0500

    footer main

---
+++ src/pages/FooterMain.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const FooterMain = () => {
+    return (
+        <footer id="footer-main">                
+            <div className="copyright-main glowInf" id="copy-animacion-main">© LSD LAB 2020</div>
+        </footer>
+        
+    )
+}
