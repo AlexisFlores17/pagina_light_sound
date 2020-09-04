f72408c6edbb65c4deccc0af75a660fb062e73cc
dstprojects <36209165+dstprojects@users.noreply.github.com>
2020-09-04 03:47:36 -0500

    animacion scroll conocenos

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -7,7 +7,7 @@ import { Navbar, Nav } from "react-bootstrap";
 const Header2 = ({ history}) => {
 
   return (
-    <div className= "header-navbar lax" data-lax-opacity="0 1, (vh/2) 0">
+    <div className= "header-navbar">
       <Navbar bg="none" expand="lg" variant="dark" className="navbar-container">
         <Navbar.Brand href="/#home">LSD LAB.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
