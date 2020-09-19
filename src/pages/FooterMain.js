bc107aa78ef377fbc80148560d04f87203dc8bab
Alex <alex.p.flores17@gmail.com>
2020-09-18 20:56:31 -0500

    aviso footer

--- src/pages/FooterMain.js
+++ src/pages/FooterMain.js
@@ -5,7 +5,7 @@ export const FooterMain = () => {
     return (
         <footer id="footer-main">                
             <div className="copyright-main glowInf" id="copy-animacion-main">© LSD LAB {new Date().getFullYear()}</div>
-            <Link to="/Aviso">Aviso de Privacidad</Link>
+            <div className="aviso"><Link className="aviso-t" to="/Aviso">Aviso de Privacidad</Link></div>
 
         </footer>
         
