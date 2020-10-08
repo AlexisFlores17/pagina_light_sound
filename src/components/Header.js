220ccc714920e11b7b9b970e91590eadae763cec
lorenzoajt <lorenzoajt@gmail.com>
2020-10-07 20:04:56 -0500

    fixing scroll with gsap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import { gsap } from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
 
 
 
 
 const Header = ({ history}) => {
+  gsap.registerPlugin(ScrollToPlugin);
 
   return (
     <div className= "header-navbar lax" data-lax-opacity="0 1, (vh/2) 0">
@@ -13,9 +17,9 @@ const Header = ({ history}) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#conocenos-sect">Conocenos</Nav.Link>
-            <Nav.Link href="#showreel-sect">Showreel</Nav.Link>
-            <Nav.Link href="#contacto-sect">Contacto</Nav.Link>
+            <Nav.Link onClick={() => gsap.to(window, {duration: 0.1, scrollTo:"#conocenos-sect"})}>Conocenos</Nav.Link>
+            <Nav.Link onClick={() => gsap.to(window, {duration: 0.1, scrollTo:"#showreel-sect"})}>Showreel</Nav.Link>
+            <Nav.Link onClick={() => gsap.to(window, {duration: 0.1, scrollTo:"#contacto-sect"})}>Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
